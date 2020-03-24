d37e2027479af0904721e43c0ebfbbdee2aa668d
xlz122 <122874722@qq.com>
2020-03-24 17:10:33 +0800

    修改antd导致部分样式被覆盖问题

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import store from '@store';
 import { Provider } from 'react-redux';
 import AppRouter from '@router';
-// import 'antd/dist/antd.css';
 import './index.css';
 
 ReactDOM.render(
