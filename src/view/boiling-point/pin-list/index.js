17225b9dfbc26a6d1547c7dcff239b78a39fc626
xlz122 <122874722@qq.com>
2020-03-27 17:10:20 +0800

    解决沸点页面控制台报错问题（ 属性大小写以及class没有使用className）

--- src/view/boiling-point/pin-list/index.js
+++ src/view/boiling-point/pin-list/index.js
@@ -30,38 +30,38 @@ function PinList() {
         </ul>
         <div className="item-action-row">
           <div className="like-action">
-            <div class="action-title-box">
-              <svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" class="icon like-icon">
-                <g fill="none" fill-rule="evenodd">
+            <div className="action-title-box">
+              <svg className="icon like-icon" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+                <g fill="none" fillRule="evenodd">
                   <path d="M0 0h20v20H0z"></path>
-                  <path stroke="#8A93A0" stroke-linejoin="round" d="M4.58 8.25V17h-1.4C2.53 17 2 16.382 2 15.624V9.735c0-.79.552-1.485 1.18-1.485h1.4zM11.322 2c1.011.019 1.614.833 1.823 1.235.382.735.392 1.946.13 2.724-.236.704-.785 1.629-.785 1.629h4.11c.434 0 .838.206 1.107.563.273.365.363.84.24 1.272l-1.86 6.513A1.425 1.425 0 0 1 14.724 17H6.645V7.898C8.502 7.51 9.643 4.59 9.852 3.249A1.47 1.47 0 0 1 11.322 2z"></path>
+                  <path stroke="#8A93A0" strokeLinejoin="round" d="M4.58 8.25V17h-1.4C2.53 17 2 16.382 2 15.624V9.735c0-.79.552-1.485 1.18-1.485h1.4zM11.322 2c1.011.019 1.614.833 1.823 1.235.382.735.392 1.946.13 2.724-.236.704-.785 1.629-.785 1.629h4.11c.434 0 .838.206 1.107.563.273.365.363.84.24 1.272l-1.86 6.513A1.425 1.425 0 0 1 14.724 17H6.645V7.898C8.502 7.51 9.643 4.59 9.852 3.249A1.47 1.47 0 0 1 11.322 2z"></path>
                 </g>
               </svg>
-              <span class="action-title">1</span>
+              <span className="action-title">1</span>
             </div>
           </div>
           <div className="comment-action">
-            <div class="action-title-box">
-              <svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" class="icon comment-icon">
-                <g fill="none" fill-rule="evenodd">
+            <div className="action-title-box">
+              <svg className="icon comment-icon" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+                <g fill="none" fillRule="evenodd">
                   <path d="M0 0h20v20H0z"></path>
-                  <path stroke="#8A93A0" stroke-linejoin="round" d="M10 17c-4.142 0-7.5-2.91-7.5-6.5S5.858 4 10 4c4.142 0 7.5 2.91 7.5 6.5 0 1.416-.522 2.726-1.41 3.794-.129.156.41 3.206.41 3.206l-3.265-1.134c-.998.369-2.077.634-3.235.634z"></path>
+                  <path stroke="#8A93A0" strokeLinejoin="round" d="M10 17c-4.142 0-7.5-2.91-7.5-6.5S5.858 4 10 4c4.142 0 7.5 2.91 7.5 6.5 0 1.416-.522 2.726-1.41 3.794-.129.156.41 3.206.41 3.206l-3.265-1.134c-.998.369-2.077.634-3.235.634z"></path>
                 </g>
               </svg>
-              <span class="action-title">10</span>
+              <span className="action-title">10</span>
             </div>
           </div>
           <div className="share-action">
-          <div class="action-title-box">
-            <svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" class="icon share-icon">
-              <g fill="none" fill-rule="evenodd">
+          <div className="action-title-box">
+            <svg className="icon share-icon" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+              <g fill="none" fillRule="evenodd">
                 <path d="M0 0h20v20H0z"></path>
-                <g stroke="#8A93A0" stroke-linecap="round" stroke-linejoin="round">
+                <g stroke="#8A93A0" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M10 2.5v10M13.5 7h2.001c.552 0 .999.437.999.96v8.621c0 1.919-1.447 1.919-1.999 1.919H5.5c-.552 0-1.999 0-1.999-1.919v-8.62c0-.53.443-.961.999-.961H6.5M7.519 4.538L10.019 2l2.5 2.513"></path>
                 </g>
               </g>
             </svg>
-            <span class="action-title">分享</span></div>
+            <span className="action-title">分享</span></div>
           </div>
         </div>
       </li>
