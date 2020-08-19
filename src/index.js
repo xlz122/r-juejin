c4cb4e14dc2cd0be91023820423c99fe7fbc277c
xlz122 <122874722@qq.com>
2020-08-19 16:31:43 +0800

    index.js提供器注释

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import store from '@store';
+// 引入react-redux提供器
 import { Provider } from 'react-redux';
 import AppRouter from '@router';
 // 引入全局方法
